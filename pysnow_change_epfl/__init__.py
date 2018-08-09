508467fe5d2efac39d5fb56a13983e9355028461
obieler <olivier.bieler@epfl.ch>
2018-08-09 15:16:33 +0200

    flake fix

--- pysnow_change_epfl/__init__.py
+++ pysnow_change_epfl/__init__.py
@@ -1,5 +1,6 @@
 """
-    (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018
+    (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
+    Switzerland, VPSI, 2018
 """
 
 __version__ = '0.0.1'
