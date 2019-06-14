8fe12be925ce2ba7e9b0f203c51de725dba13ec4
Olivier Bieler <olivier.bieler@epfl.ch>
2019-06-14 16:29:50 +0200

    bump version

--- pysnow_change_epfl/__init__.py
+++ pysnow_change_epfl/__init__.py
@@ -1,6 +1,6 @@
 """
     (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
-    Switzerland, VPSI, 2018
+    Switzerland, VPSI, 2018-2019
 """
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
