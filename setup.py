03f2a37fb3e00ca9305972f62b77f18ae79a7e93
Olivier Bieler <olivier.bieler@epfl.ch>
2019-09-30 15:39:11 +0200

    extends requirements versions

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
 
     license="Apache License 2.0",
     install_requires=[
-        'requests==2.21.0',
-        'lxml==3.7.3',
-        'zeep==2.5.0',
+        'requests>=2.21.0,<3.0',
+        'lxml>=3.7.3,<4.0',
+        'zeep>=2.5.0,<3.0',
     ],
     packages=['pysnow_change_epfl'],
     zip_safe=False
