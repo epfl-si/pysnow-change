11dccd438096e8603f3b11bfe1e624b66ad6c607
Christian Clauss <cclauss@me.com>
2019-10-07 18:42:09 +0500

    Use print() function in both Python 2 and Python 3 (#23)
    * Use print() function in both Python 2 and Python 3
    
    Legacy __print__ statements are syntax errors in Python 3 but __print()__ function works as expected in both Python 2 and Python 3.
    
    * Travis CI: Add Python 3 to the testing of the whole repo
    
    Includes lessons learned in both #24 and #25
    
    * raw_input() was removed in Python 3

--- example/example1.py
+++ example/example1.py
@@ -4,10 +4,16 @@
 """
 (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018.
 """
+from __future__ import print_function
 
-from pysnow_change_epfl.change_utils import check_sciper, check_pwd, \
-                                            get_changelog_info, \
-                                            create_change
+from pysnow_change_epfl.change_utils import (check_sciper, check_pwd,
+                                             get_changelog_info,
+                                             create_change)
+
+try:
+    raw_input
+except NameError:  # raw_input() was removed in Python 3
+    raw_input = input
 
 # Verification before create change
 try:
@@ -18,20 +24,20 @@ try:
     # with ### and the version number X.Y.Z should precede the date! **
     version_number, description, impact_category = get_changelog_info(
         '<path_to_CHANGELOG.md>')
-    print "Version: {}".format(version_number)
-    print "List of changes: \n{}".format(description)
+    print("Version: {}".format(version_number))
+    print("List of changes: \n{}".format(description))
     rep = raw_input(
         "Is the version and description correct? [type 'yes' to confirm] : ")
     if rep != 'yes':
-        print "create change canceled."
+        print("create change canceled.")
         exit(1)
 except Exception as exc:
-    print exc
+    print(exc)
     exit(1)
 
 # Create change
-print create_change(service_id='SVC0016',
+print(create_change(service_id='SVC0016',
                     snow_group='SI_NEWS',
                     impact_category=impact_category,
                     short_description="Actu - {}".format(version_number),
-                    description=description)
+                    description=description))
